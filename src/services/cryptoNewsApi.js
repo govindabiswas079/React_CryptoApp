363621895bf2461a2bc431cf6733a333b061af43
prem-pb <govindbiswas079@gmail.com>
2021-09-06 00:30:38 +0530

    first commit

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': 'b5a39355camsh0a53a4f0b27f4acp15f3eejsnf2b383d38e97'
+};
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com/'
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        }),
+    }),
+})
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
